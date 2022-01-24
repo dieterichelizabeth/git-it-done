f36f15ae6ceb7f55d295c2d12129e9e263220a25
dieterichelizabeth <dieterichelizabeth@gmail.com>
2022-01-24 17:05:58 -0600

    updated request logic with dynamic variables

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,27 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-  };
+// user parameter w/in the getUserRepos function
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
-  getUserRepos();
+    // Response object in the fetch logic with method called json which formats the response
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+    // The jason method then returns another promise which callback function captures the data
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+};
+
+// call the getUserRepos function passing my username
+getUserRepos("dieterichelizabeth");
+console.log("outside");
+
+
+
+  /*
+  Notes: 
+  You can liken an API endpoint to a function in the sense that the same base URL 
+  can return different data depending on the argument(s) given.
+  getUserRepos(user); // https://api.github.com/users/<user>/repos
+  */
